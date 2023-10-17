710ba493a8f368738d31a8daea1bd86e0773fc66
DeepaV22 <deepav2208@gmail.com>
2023-10-17 14:49:44 +0530

    Logic addition
    linked dijkstra to adaptive
    * created traffic prediction list
    * infinte loop while using dij(0,1 )
    *logic error in main return

--- new_adaptive.py
+++ new_adaptive.py
@@ -1,44 +1,65 @@
 import main
 import dijkstra
+import virtual_channel
 
-dict1 = {
-    0: (0, 0), 1: (0, 1), 2: (0, 2), 3: (0, 3),
-    4: (1, 0), 5: (1, 1), 6: (1, 2), 7: (1, 3),
-    8: (2, 0), 9: (2, 1), 10: (2, 2), 11: (2, 3),
-    12: (3, 0), 13: (3, 1), 14: (3, 2), 15: (3, 3)
-}
+def adaptive_route(src,dest):
 
-blocked_node = main.node1
-print("blocked_node is ",blocked_node)
-print("neighboring nodes are: ", main.value)
+    dict1 = {
+        0: (0, 0), 1: (0, 1), 2: (0, 2), 3: (0, 3),
+        4: (1, 0), 5: (1, 1), 6: (1, 2), 7: (1, 3),
+        8: (2, 0), 9: (2, 1), 10: (2, 2), 11: (2, 3),
+        12: (3, 0), 13: (3, 1), 14: (3, 2), 15: (3, 3)
+    }
 
-adj = main.value
-traffic_prediction = []
-for i in adj:
-    print("in new adaptive")
-    coordinate = dict1[i]
-    print(main.available(main.node1,i))
-    if main.available_bit_space >= main.n:
-        traffic_prediction.append(1)
+    blocked_node = main.node1
+    print("blocked_node is ",blocked_node)
+    print("neighboring nodes are: ", main.value)
+
+    adj = main.value
+    traffic_prediction = []
+    for i in adj:
+        print("in new adaptive")
+        coordinate = dict1[i]
+        print(main.available(main.node1,i))
+        if main.available_bit_space >= main.n:
+            traffic_prediction.append(1)
+        else:
+            traffic_prediction.append(0)
+    print("traffic prediction list= ",traffic_prediction)
+
+    count_1 = 0
+    count_0 = 0
+    for i in traffic_prediction:
+        if traffic_prediction[i] == 1:
+            count_1 += 1
+        elif traffic_prediction[i] == 0:
+            count_0 +=1
+
+
+
+    traffic_prediction = [1,0]
+    allow = []
+    for j in range(len(adj)):
+        if adj[j]*traffic_prediction[j] != 0:
+            # print("adj[j] : ",adj[j])
+            # print("traffic_prediction[j] : ",traffic_prediction[j])
+            allow.append(adj[j]*traffic_prediction[j])
+
+    print("allow ", allow)
+    # if count_1 <= len(traffic_prediction):
+    #     dijkstra.print_paths(main.node1, main.dest, allow)
+
+    if len(allow) !=0:
+        next_node = dijkstra.print_paths(main.node1, main.dest, allow)
+        allow =[]
+        while next_node != main.dest:
+            adaptive_route(next_node, main.dest)
     else:
-        traffic_prediction.append(0)
-    print(traffic_prediction)
-
-count_1 = 0
-count_0 = 0
-for i in traffic_prediction:
-    if traffic_prediction[i] == 1:
-        count_1 += 1
-    elif traffic_prediction[i] == 0:
-        count_0 +=1
-
-if count_1 <= len(traffic_prediction):
-    dijkstra.print_paths
-
-elif count_0 == len(traffic_prediction):
-    vc()
+        virtual_channel.create_virtual_channel()
+    # elif count_0 == len(traffic_prediction):
+    #     vc()
 
 
-        
+            
 
 
--- virtual_channel.py
+++ virtual_channel.py
@@ -5,21 +5,23 @@
 # import final_traffic
 # import adaptive
 
-virtual_control[0,0,0,0]                # virtual nodes 5,6,9,10
-virtual_connections = {5:[0,2,8,10],
-                       6:[1,3,9,11],
-                       9:[4,6,12,14],
-                       10:[5,7,13,15]
-                       }
+def create_virtual_channel():
+    print("inside virtual channel")
+    virtual_control=[0,0,0,0]                # virtual nodes 5,6,9,10
+    virtual_connections = {5:[0,2,8,10],
+                        6:[1,3,9,11],
+                        9:[4,6,12,14],
+                        10:[5,7,13,15]
+                        }
 
-deadlock_node = 3      # adaptive.deadlock_node shld be pulled from adaptive file
+    deadlock_node = 3      # adaptive.deadlock_node shld be pulled from adaptive file
 
-for key,value in virtual_connections.items():
-    for i in range(len(value)):
-        if deadlock_node == value[i]:
-            virtual_channel = key
-            print(key)
-            break
+    for key,value in virtual_connections.items():
+        for i in range(len(value)):
+            if deadlock_node == value[i]:
+                virtual_channel = key
+                print(key)
+                break
 
 
 
