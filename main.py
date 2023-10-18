06ce9c020e2ba4495b6bfe386db2ec4689e5b7f6
DeepaV22 <deepav2208@gmail.com>
2023-10-18 14:02:45 +0530

    Fixed circular import error
    1) Combined new.py and new_adaptive.py
    
    2) adaptive routing ran in loop
    
    3) yet to define virtual channel

--- main.py
+++ main.py
@@ -2,15 +2,78 @@ import numpy as np
 import random
 import math
 import time
-#import random_traffic
-# import adaptive_traffic
-#import algorithm
 import final_traffic
-import new_adaptive
+# import new_adaptive
 import dijkstra
-
+import virtual_channel
 
 dest_reached = False
+
+def adaptive_route(src,dest,n,value):
+
+    dict1 = {
+        0: (0, 0), 1: (0, 1), 2: (0, 2), 3: (0, 3),
+        4: (1, 0), 5: (1, 1), 6: (1, 2), 7: (1, 3),
+        8: (2, 0), 9: (2, 1), 10: (2, 2), 11: (2, 3),
+        12: (3, 0), 13: (3, 1), 14: (3, 2), 15: (3, 3)
+    }
+
+    blocked_node = src
+    print("blocked_node is ",blocked_node)
+    print("neighboring nodes are: ", value)
+
+    adj = value                              # chumma instead of changing everything, keep like this only
+    traffic_prediction = []
+    for i in adj:
+        print("in new adaptive")
+        coordinate = dict1[i]
+        #print(main.available(main.node1,i))
+        available_bit_space = available(src,i)
+        if available_bit_space >= n:
+            traffic_prediction.append(1)
+        else:
+            traffic_prediction.append(0)
+    print("traffic prediction list= ",traffic_prediction)
+
+    count_1 = 0
+    count_0 = 0
+    for i in range(len(traffic_prediction)):
+        if traffic_prediction[i] == 1:
+            count_1 += 1
+        elif traffic_prediction[i] == 0:
+            count_0 +=1
+
+
+
+    #traffic_prediction = [1,0]
+    allow_adj = []
+    for j in range(len(adj)):
+        if adj[j]*traffic_prediction[j] != 0:
+            # print("adj[j] : ",adj[j])
+            # print("traffic_prediction[j] : ",traffic_prediction[j])
+            allow_adj.append(adj[j]*traffic_prediction[j])
+
+    print("allow_adj ", allow_adj)
+    # if count_1 <= len(traffic_prediction):
+    #     dijkstra.print_paths(main.node1, main.dest, allow_adj)
+
+    if len(allow_adj) !=0:
+        next_node = dijkstra.print_paths(src, dest, allow_adj)
+        allow_adj.clear()
+        traffic_prediction.clear()
+        #print("new next node",next_node)
+        print("allow_adj : empty",allow_adj)
+        while next_node != dest:
+             #adaptive_route(next_node, main.dest)
+             # main.xy_main(next_node, main.dest,main.n)
+             print("hello")
+    else:
+        virtual_channel.create_virtual_channel()
+    # elif count_0 == len(traffic_prediction):
+    #     vc()
+
+
+        
 # Delays
 '''
 n0_1 = 4
@@ -120,6 +183,7 @@ print("Node Assignment : \n", dict1)
 
 #print("Path: ", path)
 
+# def xy_routing(src, dest, n):
 s = dict1[src]
 d = dict1[dest]
 
@@ -156,6 +220,8 @@ path.append([dx, dy])
 
 print("Path: ", path)
 
+
+
 def get_neighbors(node, rows, columns):
     i, j = node
     neighbors = []
@@ -180,6 +246,7 @@ def get_neighbors(node, rows, columns):
 total_delay = 0.0
 total_traffic = 0.0
 
+
 for i in range(len(path) - 1):
     x1, y1 = path[i]
     x2, y2 = path[i + 1]
@@ -190,9 +257,11 @@ for i in range(len(path) - 1):
     # packet_bits=int(input("Enter packets(in bits):"))
     t0_1, t1_0, t1_2, t2_1, t2_3, t3_2, t0_4, t4_0, t4_5, t5_4, t1_5, t5_1, t5_6, t6_5, t2_6, t6_2, t6_7, t7_6, t3_7, t7_3, t4_8, t8_4, t8_9, t9_8, t5_9, t9_5, t6_10, t10_6, t9_10, t10_9, t7_11, t11_7, t11_10, t10_11, t12_13, t13_12, t8_12, t12_8, t9_13, t13_9, t10_14, t14_10, t13_14, t14_13, t14_15, t15_14, t11_15, t15_11 = next(
         data_generator)
-    # print(t0_1)
+    print(t0_1)
+
 
     def available(node1, node2):
+        # node_traffic(path)
         tf_link = eval(f"t{node1}_{node2}")  # value
         # print(f"The amount of traffic genarated randomly in the link is : { tf_link}")
         tf_link_bits = math.floor(tf_link)  # traffic generated randomly in bits
@@ -218,17 +287,18 @@ for i in range(len(path) - 1):
 
         neighbor_list = get_neighbors(dict1[node1],4,4)
         for final_node in neighbor_list:
-             #print(final_node)
-             for i in dict1 :
+            #print(final_node)
+            for i in dict1 :
                 
-                 if dict1[i] == final_node:
-                      value.append(i)
-                      
+                if dict1[i] == final_node:
+                    value.append(i)
+                    
         value.remove(node2)
-        new_adaptive.adaptive_route(node1, dest)
-        # dijkstra.print_paths(node1, dest, value)
+        adaptive_route(node1,dest, n, value)
+            # dijkstra.print_paths(node1, dest, value)
         break
         #adaptive.hello(node1,dest,n)
+
 
     # delay = eval(f"n{node1}_{node2}")
     # total_delay += delayF
@@ -238,3 +308,5 @@ for i in range(len(path) - 1):
 #     print("Total Delay: ", total_delay)
 # else:
 #     print(f"Destination could not be reached, Total Delay=??")
+
+# xy_routing(src, dest, n)
